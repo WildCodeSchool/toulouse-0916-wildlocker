1080370756f29c6da20747c4d839346c2aa9edf8
lucas <lucas.suquet@gmail.com>
2016-09-27 13:24:22 +0200

    parrametre firebase

--- app/src/main/java/fr/wildcodeschool/apprenti/lockthedoor/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/apprenti/lockthedoor/MainActivity.java
@@ -7,10 +7,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_LOGIN = "user_login"; //On déclare les deux données de log */
     public final static String EXTRA_PASSWORD = "user_password";
+
+    // récupération de la Fire base
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+    // Récupération de la table
+    private DatabaseReference rootTable = FirebaseDatabase.getInstance().getReference().child("users");
+    //écouté les donné de ma base
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,32 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra(EXTRA_LOGIN, login.getText().toString());
                 intent.putExtra(EXTRA_PASSWORD, pass.getText().toString()); // c'est ici qu'on réécupère et associe les données a l'intent */
                 startActivity(intent); // ici on lance l'autre activité */
+
+            }
+        });
+        FirebaseDatabase.getInstance().getReference().addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
