b93e5d9d7b3cc63e5f52705605f0d20e61aad5a5
lucas <lucas.suquet@gmail.com>
2016-10-12 14:01:04 +0200

    commit Web serveur Raspberry

--- app/src/main/java/fr/wildcodeschool/apprenti/lockthedoor/DoorActivity.java
+++ app/src/main/java/fr/wildcodeschool/apprenti/lockthedoor/DoorActivity.java
@@ -2,11 +2,14 @@ package fr.wildcodeschool.apprenti.lockthedoor;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,6 +18,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
@@ -23,66 +33,96 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class DoorActivity extends AppCompatActivity {
 
-    public Button unlock;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser user;
-    public DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
     public DatabaseReference rootUsers =  FirebaseDatabase.getInstance().getReference().child("Lockthedor");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        unlock = (Button) findViewById(R.id.button2);
         mFirebaseAuth = FirebaseAuth.getInstance();
         user = mFirebaseAuth.getCurrentUser();
+
+        /*rootUsers.child(user.getUid()).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                User me = dataSnapshot.child("lieu").getValue(User.class);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        }); */
         if (user == null) {
             Intent intent = new Intent(this, AuthActivity.class);
             startActivity(intent);
             finish();
         }
+        /********************************/
+         /*    Define all the buttons    */
+        /********************************/
+        ToggleButton led = (ToggleButton) findViewById(R.id.Led);
 
-        else{
-            unlock.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    rootUsers.addChildEventListener(new ChildEventListener() {
-                        @Override
-                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                            String Author = dataSnapshot.child("lieu").getValue().toString();
-                        }
 
-                        @Override
-                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-                        }
-
-                        @Override
-                        public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                        }
-
-                        @Override
-                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-                        }
-                    });
+        /*******************************************************/
+         /*  Set an onclick/onchange listener for every button  */
+        /*******************************************************/
+        led.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    new Background_get().execute("led2=1");
+                    new Background_get().execute("led1=0");
+                } else {
+                    new Background_get().execute("led2=0");
+                    new Background_get().execute("led1=1");
                 }
-            });
-
-
-        }
-
-
-
-
-
+            }
+        });
 
     }
+    // Connection Raspberry
+    private class Background_get extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                /* Change the IP to the IP you set in the arduino sketch */
+                URL url = new URL("http://192.168.1.30/?" + params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder result = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null)
+                    result.append(inputLine).append("\n");
+
+                in.close();
+                connection.disconnect();
+                return result.toString();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
+
 
---
+++ app/src/main/java/fr/wildcodeschool/apprenti/lockthedoor/User.java
@@ -0,0 +1,11 @@
+package fr.wildcodeschool.apprenti.lockthedoor;
+
+/**
+ * Created by apprenti on 12/10/16.
+ */
+public class User {
+    public String school;
+    public User(String school){
+        this.school = school;
+    }
+}
